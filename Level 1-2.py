3a6cd33191e08bcaa0021e3afa0c051c9d9c0a31
Leejinho <dovmf0650@naver.com>
2020-08-07 09:42:37 +0900

    Level 1

---
+++ Level 1-2.py
@@ -0,0 +1,8 @@
+# 전화번호 보안성
+
+a=list(input())                 # 전화번호 입력 / 리스트로 저장
+for i in range(len(a)-4):       # 입력받은 전화번호에서 끝의 네자리를 제외하고 * 로 변경
+    a[i]='*'
+
+for i in range (len(a)):        # 전화번호 출력
+    print(a[i], end = '')
